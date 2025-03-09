64eb195fb90d41da5e262e6f8b621ce56d70c760
kim-seung-eun <adrea76@gmail.com>
2025-03-09 10:52:34 +0900

    타이틀 변경

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
         desc:'Welcome to frontEnd!!'
       },
       subject : {
-        title: "프론트엔드 개발자" ,
+        title: "프론트엔드 개발자1" ,
         desc: "기본언어인 html, css, javascript부터 학습합니다. ",
       },
       menus:[
