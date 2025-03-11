e561ffadd857a39cc7151c35aa89b692a7b0fc82
kim-seung-eun <adrea76@gmail.com>
2025-03-11 19:43:19 +0900

    함수형 으로 재적용처리

--- src/component/MyNav.js
+++ src/component/MyNav.js
@@ -1,62 +1,37 @@
-import React, { Component } from 'react'
+import React, { Component, useEffect , useState} from 'react'
 
-class MyNav extends Component {
-    shouldComponentUpdate(newProps, newState){
-        console.log('shouldComponentUpdate 작동', 
-            newProps.data, // 변경된 값
-            this.props.data // 변경전 값
-            );
-        if(newProps.data === this.props.data){
-            return false;
-        }
-        
-        return true;        
-        
-    }
-    render() {
-        console.log('MyNav 실행');
-        let lists = [];
-        //console.log(this.props.data);
-        let data = this.props.data;
-        /*
-        ## 1. while 문 사용 
-        let i=0;
-        while(i < data.length){
-            lists.push(
-                <li key={data[i].id}><a href="/">{data[i].title}</a></li>
-            );
-            i++;
-        }
-
-        //  ## 2. forEach 문 사용 
-        data.forEach(function (item, idx){
-            lists.push(
-                <li key={item.id}><a href="/">{item.title}</a></li>
-            ); 
-        });
-        */
-
-        //##  3. forEach 문 사용 
+const MyNav = ({data, onChangePage})=>{
+    const[list, setList] = useState([]);
+    let lists = [];
+    const getList = () =>{
         data.forEach(item => {
             lists.push(
                 <li key={item.id}>
                     <a href="/"                    
                     onClick={e=>{
                         e.preventDefault();
-                        this.props.onChangePage(item.id);
+                        onChangePage(item.id);
                     }}>{item.title}</a>
                 </li>
             ); 
         });
+        setList(lists);
+    }
 
-        return (
+    useEffect(()=>{
+        getList();
+        console.log('getList실행');
+    }, [data]
+    )
+
+    return (
         <nav>
             <ul>
-            {lists}
+            {list}
             </ul>
-        </nav>      
-        )
-    }
+        </nav>
+    )
+    
 }
 
 export default MyNav;
